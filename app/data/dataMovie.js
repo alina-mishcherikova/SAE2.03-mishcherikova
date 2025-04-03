c03cff7b7ba39a7759dd8d8ec77516f35a9ff3c8
Mishcherikova Alina <mishcherikova1@mmi-dev.unilim.fr>
2025-04-03 11:58:21 +0200

    FRONT Itération 1 maj DataMovie

--- app/data/dataMovie.js
+++ app/data/dataMovie.js
@@ -2,16 +2,10 @@ let HOST_URL = "https://mmi.unilim.fr/~mishcherikova1/SAE2.03-mishcherikova/";
 
 let DataMovie = {};
 
- /**
-     * @param {string} name - The week parameter to be sent to the server.
-     * @param {string} img - The day parameter to be sent to the server.
-     * @returns The response from the server.
-     * 
-**/
-DataMovie.requestMovies = async function(name, img){
-    let answer = await fetch(HOST_URL + "/server/script.php?name=" + name + "&img=" + img);
+DataMovie.requestMovies = async function () {
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=readmovies");
     let data = await answer.json();
     return data;
 }
 
-export {DataMovie};
+export { DataMovie };
