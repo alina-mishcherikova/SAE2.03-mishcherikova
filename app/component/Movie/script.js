c7bfe03386542500ac38f0120abd82a652987931
Mishcherikova Alina <mishcherikova1@mmi-dev.unilim.fr>
2025-04-03 15:46:16 +0200

    update Front iteration1

--- app/component/Movie/script.js
+++ app/component/Movie/script.js
@@ -1,12 +1,24 @@
 let templateFile = await fetch("./component/Movie/template.html");
 let template = await templateFile.text();
 
+const templateFile2 = await fetch("./component/Movie/templateCard.html");
+const templateCards = await templateFile2.text();
+
+
 let MovieCard = {};
 
-MovieCard.format = function (MoviemName, MovieImg) {
+MovieCard.format = function (obj) {
   let html = template;
-  html = html.replace("{{MovieName}}", MovieName);
-  html = html.replace("{{MovieImg}}", MovieImg);
+  let cardsHTML = "";
+
+  for (let c of obj) {
+    let card = templateCards;
+    card = card.replace("{{name}}", c.name);
+    card = card.replace("{{image}}", c.image);
+    cardsHTML += card;
+  }
+
+  html = html.replace("{{cards}}", cardsHTML);
   return html;
 };
 
