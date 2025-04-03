0ce8bb7f2b8c143c353f97fba4e20db9c5ea6baf
Alina Mishcherikova <alina.mishcherikova@etu.unilim.fr>
2025-04-03 10:37:49 +0200

    Maj credentials bd

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "mishcherikova1");
+define("DBLOGIN", "mishcherikova1");
+define("DBPWD", "mishcherikova1");
